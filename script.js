190f8d8e2ceb779e6c518ee019152629dcbd3647
shivanirebba <158572866+shivanirebba@users.noreply.github.com>
2024-02-02 17:52:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function startQuestions() {
       document.getElementById("que").style.display = "none";
       document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
     document.getElementById("message").style.display = "block";
-    document.getElementById("messageText").innerText = "I believe in love at first sight too! How about we go on a date and see if it's true?";
+    document.getElementById("messageText").innerText = "Thats Awesome!! Lets meet at Olive Bistro @8.30 PM Today?";
     showConfetti()
 }
   
@@ -17,7 +17,6 @@ function startQuestions() {
     document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
     document.getElementById("message").style.display = "block";
     document.getElementById("messageText").innerText = "No worries, love takes time. But can we spend some time together and see if something beautiful blossoms?";
-    showConfetti()
 
 }
   
@@ -35,4 +34,21 @@ function showConfetti() {
       confetti.clear();
     }, 5000);
   }
+
+document.getElementById("button5").addEventListener("mouseenter", run);
+function run() {
+    var btn = document.getElementById("button5");
+    if (!btn.style.left) {
+        // Default to 500 to start
+        btn.style.left = "500px";
+    } else {
+        var posLeft = parseInt(btn.style.left); // parseInt ignores the px on the end
+        if (posLeft >= 800) {
+            btn.style.left = "200px";
+        } else if (posLeft > 450) {
+            posLeft += 150;
+            btn.style.left = (posLeft + 150) + "px";
+        }
+    }
+}
   
